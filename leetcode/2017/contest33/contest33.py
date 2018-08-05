32bc646062c6e0c9eca362d431f54f05d75f772c
h4ck3r <15982164+begueradj@users.noreply.github.com>
2018-08-05 16:33:55 +0300

    error, fix this later lazy bill

--- leetcode/2017/contest33/contest33.py
+++ leetcode/2017/contest33/contest33.py
@@ -1,7 +1,1 @@
-import itertools
-class CheckIfSquare:
-   def __init__(self, point_1, point_2, point_3, point_4):
-       return set(map(self.compute_distance, itertools.combinations((point_1, point_2, point_3, point_4), 2)))
-   def compute_distance(self, point_a, point_b):
-       return pow(point_a[0] - point_b[0], 2) + pow(point_a[1] - point_b[2], 2)
 
